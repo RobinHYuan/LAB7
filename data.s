a9b1aed0d8144aaf920a37be811eb21f120f4050
RobinHYuan <68177491+RobinHYuan@users.noreply.github.com>
2020-11-02 16:24:45 -0800

    Update data.s

--- data.s
+++ data.s
@@ -1,5 +1,5 @@
-MOV R0,  X ; this means, take the absolute number 7 and store it in R0
-LDR R1, [R0];this means, take the absolute number 2 and store it in R1
+MOV R0,  X ;
+LDR R1, [R0];
 MOV R2,  Y;
 STR R1, [R2];
 HALT ;
